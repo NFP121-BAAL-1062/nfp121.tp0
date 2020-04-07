1bbcfac68bb1921cfde8e46eeb4b179aa881b2e8
Haidar Youness <husseinhaidar.youness@isae.edu.lb>
2020-04-07 17:36:34 +0300

    TP0 Done

---
+++ TP0/MyClass.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class MyClass here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyClass extends YourClass
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    HisClass his;
+
+    /**
+     * Constructor for objects of class MyClass
+     */
+    public MyClass()
+    {
+        // initialise instance variables
+        x = 0;
+        his=null;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
